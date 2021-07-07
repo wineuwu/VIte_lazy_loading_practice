507a9df623942cf9fdb51fce35a20455c1ff2147
winnie <asgh03226@gmail.com>
2021-07-07 11:42:42 +0800

    練習 IntersectionObserver api 監聽元素的出現 與消失

--- main.js
+++ main.js
@@ -1,28 +1,53 @@
-const picDom = document.querySelector('.showPic .container');
+import './scss/main.scss'
+
+const picDom = document.querySelector('.showPic');
+const pic = picDom.querySelectorAll('.container .pic-col img');
+
 const API_TOKEN = import.meta.env.VITE_PEXEL_API_TOKEN;
 const url =`https://api.unsplash.com/photos/?client_id=${API_TOKEN}&per_page=20`;
 
-const creatImg = async () => {
-  const resp = await requestData(url) ;
-  const img =resp.map(item => {
-    return `
-    <img src="${item.urls.small}">
-  `
-  }).join('');
-  picDom.insertAdjacentHTML("beforeend",img);
-}
 
-const requestData = (url) =>{
-  const response = fetch(url,{
-    headers:{
-      'Authorization': `${API_TOKEN}`
+const lazyLoading =(entry) => {
+  entry.forEach((img,idx) => {
+    if(img.isIntersecting){
+      console.log(img.target);
+      observer.unobserve(img.target)
     }
-  }).then(res => res.json()).then(item => item)
+  
+  })
+ 
+}
+const observer = new IntersectionObserver(lazyLoading, {
+  root:null,
+  rootMargin: "0px 0px 0px 0px",
+  threshold: [0]
+})
 
-  return response;
-} 
+pic.forEach(img => observer.observe(img))
 
-creatImg();
+
+// const creatImg = async () => {
+//   const resp = await requestData(url) ;
+//   const img =resp.map(item => {
+//     return `
+//     <div class="pic-col">
+//       <img src="${item.urls.small}"  loading="lazy">
+//     </div>
+//   `
+//   }).join('');
+//   picDom.insertAdjacentHTML("beforeend",img);
+// }
+
+// const requestData = (url) =>{
+//   const response = fetch(url,{
+//     headers:{
+//       'Authorization': `${API_TOKEN}`
+//     }
+//   }).then(res => res.json()).then(item => item)
+
+//   return response;
+// } 
+// creatImg();
 
 
 
